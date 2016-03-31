edd5f52bf8244a55254c8f2386f1c25ca5c2b392
AlexanderYao <dwy4670@gmail.com>
2016-03-31 18:01:57 +0800

    todo string support

--- README.md
+++ README.md
@@ -3,5 +3,7 @@ Regular Expression Parser
 
 A simple regular expression parser
 
+TODO:
 1. can only apply single char, not string, such as: 1* not 12*
-2. can only parse simple Regular Expression, such as: */+ not ^!()-[]|\$
+2. can only parse simple Regular Expression, such as: */+ not ^!()-[]|{}\?$
+3. support special char set, such as: \w \W \d \D 
---
+++ RegularExpressionParser.Test/TestString.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RegularExpressionParser.Test
+{
+    [TestClass]
+    public class TestString
+    {
+        private RegExp _exp;
+        public TestString()
+        {
+            _exp = new RegExp("(ab)*c");
+        }
+
+        [TestMethod]
+        public void TestString_Match()
+        {
+            Assert.IsTrue(_exp.IsMatch("ababc"));
+        }
+
+        [TestMethod]
+        public void TestString_NotMatch()
+        {
+            Assert.IsFalse(_exp.IsMatch("abac"));
+        }
+    }
+}
